e7e2d59043a908cbcdcbcf7010952a544375cf71
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-01-11 16:21:47 -0600

    🍊

--- playwright.vercel.config.ts
+++ playwright.vercel.config.ts
@@ -4,9 +4,8 @@ import commonConfig from "./playwright.config";
 const config: PlaywrightTestConfig = {
   ...commonConfig,
 
-  webServer: {
-    command: "npm run serve.express",
-    port: 3000,
+  use: {
+    baseURL: "https://qwik-city-e2e.vercel.app/",
   },
 };
 
