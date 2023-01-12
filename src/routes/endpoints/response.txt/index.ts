1d55a7643d0ff3cc3dbb91d7abb2abe15c95899b
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-01-12 13:36:13 -0600

    🐐

--- src/routes/endpoints/response.txt/index.ts
+++ src/routes/endpoints/response.txt/index.ts
@@ -2,9 +2,16 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 
 export const onRequest: RequestHandler = ({ send }) => {
   const response = new Response("responsetext", {
+    status: 201,
     headers: {
       "content-type": "text/plain; charset=utf-8",
     },
   });
+
+  console.log("response", response.status, response.statusText);
+  response.headers.forEach((value, key) => {
+    console.log("header", key, value);
+  });
+
   send(response);
 };
