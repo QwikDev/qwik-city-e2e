b573b9ba861c775c2bd6da717cb938d2b47cd0ff
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-01-24 09:37:58 -0600

    🐌

--- tests/404.spec.ts
+++ tests/404.spec.ts
@@ -1,15 +1,19 @@
 import { expect, test } from "@playwright/test";
 
-test("nested directory 404", async ({ page }) => {
-  const rsp = (await page.goto("/static/idk/"))!;
-  expect(rsp.status()).toBe(404);
+test("nested directory 404", async ({ page }, { config }) => {
+  if (config.metadata.server !== "dev") {
+    const rsp = (await page.goto("/static/idk/"))!;
+    expect(rsp.status()).toBe(404);
 
-  await expect(page.locator("h1")).toContainText("Static Directory 404");
+    await expect(page.locator("h1")).toContainText("Static Directory 404");
+  }
 });
 
-test("root directory 404", async ({ page }) => {
+test("root directory 404", async ({ page }, { config }) => {
   const rsp = (await page.goto("/idk/"))!;
   expect(rsp.status()).toBe(404);
 
-  await expect(page.locator("h1")).toContainText("Root Directory 404");
+  if (config.metadata.server !== "dev") {
+    await expect(page.locator("h1")).toContainText("Root Directory 404");
+  }
 });
