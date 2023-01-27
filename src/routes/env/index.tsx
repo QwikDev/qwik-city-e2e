fe0513451bf239407eacf9f9192508f0d09d01a5
Manu MA <manu.mtza@gmail.com>
2023-01-27 18:11:20 +0100

    🏝

--- src/routes/env/index.tsx
+++ src/routes/env/index.tsx
@@ -5,7 +5,6 @@ export const getEnv = loader$(({env}) => {
   return {
     platform: env.get('PLATFORM_NAME'),
     importMetaEnv: import.meta.env.VITE_HELLO_WORLD,
-    processEnv: process.env.HELLO_WORLD,
   }
 });
 
@@ -18,9 +17,7 @@ export default component$(() => {
       <ul>
         <li>env.value.platform: {env.value.platform}</li>
         <li>env.value.importMetaEnv: {env.value.importMetaEnv}</li>
-        <li>env.value.processEnv: {env.value.processEnv}</li>
         <li>importmeta.env.VITE_HELLO_WORLD: {importMeta}</li>
-        <li>process.env.HELLO_WORLD: {process.env.HELLO_WORLD}</li>
       </ul>
     </>
   );
