ad8f83de0860665076432c3d4805781cc1827d15
Manu MA <manu.mtza@gmail.com>
2023-02-08 18:53:10 +0100

    🐖

--- src/routes/env/index.tsx
+++ src/routes/env/index.tsx
@@ -2,7 +2,6 @@ import { component$ } from "@builder.io/qwik";
 import { DocumentHead, loader$ } from "@builder.io/qwik-city";
 
 export const getEnv = loader$(({ env }) => {
-  console.log('process.env', process.env);
   return {
     platform: env.get("PLATFORM_NAME"),
     importMetaEnv: import.meta.env.VITE_HELLO_WORLD,
