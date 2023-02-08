b8e0920564d35c96a6f54e734ce04d6da4897c26
Manu MA <manu.mtza@gmail.com>
2023-02-08 17:04:12 +0100

    🎡

--- tests/env.spec.ts
+++ tests/env.spec.ts
@@ -4,6 +4,11 @@ test("index", async ({ page }, {config}) => {
   await page.goto("/env/");
 
   await expect(page.locator('#env-value-platform')).toHaveText(config.metadata.server);
-  await expect(page.locator('#env-value-importMetaEnv')).toHaveText('foo prod');
-  await expect(page.locator('#importmeta-env-VITE_HELLO_WORLD')).toHaveText('foo prod');
+  if (config.metadata.server === 'dev') {
+    await expect(page.locator('#env-value-importMetaEnv')).toHaveText('foo dev');
+    await expect(page.locator('#importmeta-env-VITE_HELLO_WORLD')).toHaveText('foo dev');
+  } else {
+    await expect(page.locator('#env-value-importMetaEnv')).toHaveText('foo prod');
+    await expect(page.locator('#importmeta-env-VITE_HELLO_WORLD')).toHaveText('foo prod');
+  }
 });
