4d46d0db7928a7dc76a80ab556fa2c9a4884a40f
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-17 11:08:42 -0600

    🌮

--- tests/entries.spec.ts
+++ tests/entries.spec.ts
@@ -1,15 +1,23 @@
 import { expect, test } from "@playwright/test";
 
-test("custom route entry", async ({ page }) => {
+test("custom route entry", async ({ page }, { config }) => {
+  if (config.metadata.server === "dev") {
+    return;
+  }
+
   const rsp = (await page.goto("/entry.js"))!;
   expect(rsp.status()).toBe(200);
-  expect(rsp.headers()["content-type"]).toContain("application/javascript");
+  expect(rsp.headers()["content-type"]).toContain("javascript");
   expect(await rsp.text()).toContain("entry.js");
 });
 
-test("service-worker.js", async ({ page }) => {
+test("service-worker.js", async ({ page }, { config }) => {
+  if (config.metadata.server === "dev") {
+    return;
+  }
+
   const rsp = (await page.goto("/service-worker.js"))!;
   expect(rsp.status()).toBe(200);
-  expect(rsp.headers()["content-type"]).toContain("application/javascript");
+  expect(rsp.headers()["content-type"]).toContain("javascript");
   expect(await rsp.text()).toContain("QwikBuild");
 });
