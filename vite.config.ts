8b55d8a134e964b8daa4889a9bf78a2dc81612a6
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-23 10:09:54 -0600

    🐋

--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,23 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { qwikReact } from "@builder.io/qwik-react/vite";
 export default defineConfig(() => {
-    return {
-        optimizeDeps: {
-            include: ["@mui/material"],
-        },
-        plugins: [
-            qwikCity({
-                routesDir: 'custom-src/routes'
-            }),
-            qwikVite({
-                srcDir: 'custom-src'
-            }),
-            qwikReact()
-        ],
-        preview: {
-            headers: {
-                "Cache-Control": "public, max-age=600",
-            },
-        }
-    };
+  return {
+    optimizeDeps: {
+      include: ["@mui/material"],
+    },
+    plugins: [
+      qwikCity({
+        routesDir: "custom-src/routes",
+      }),
+      qwikVite({
+        srcDir: "custom-src",
+      }),
+      qwikReact(),
+    ],
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
+      },
+    },
+  };
 });
