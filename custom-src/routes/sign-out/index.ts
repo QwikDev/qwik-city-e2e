9a1184945255478124b3141c02b2f418ca83785d
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-02-23 20:09:32 -0600

    🛩

--- custom-src/routes/sign-out/index.ts
+++ custom-src/routes/sign-out/index.ts
@@ -4,5 +4,5 @@ import { AUTHTOKEN_COOKIE_NAME, USER_COOKIE_NAME } from "../../auth/auth";
 export const onGet: RequestHandler = async ({ redirect, cookie }) => {
   cookie.delete(AUTHTOKEN_COOKIE_NAME, { path: "/" });
   cookie.delete(USER_COOKIE_NAME, { path: "/" });
-  throw redirect(301, "/");
+  throw redirect(301, "/app/");
 };
