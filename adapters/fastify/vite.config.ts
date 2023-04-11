c7fb96b7e31266d1acf3df135ce4d9cb1caad3e5
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-04-11 17:18:10 +0200

    feat: fastify e2e support (#7)
    * feat: fastify e2e support
    
    * fix: missing playwright config

---
+++ adapters/fastify/vite.config.ts
@@ -0,0 +1,25 @@
+import { nodeServerAdapter } from "@builder.io/qwik-city/adapters/node-server/vite";
+import { extendConfig } from "@builder.io/qwik-city/vite";
+import baseConfig from "../../vite.config";
+
+export default extendConfig(baseConfig, () => {
+  return {
+    build: {
+      ssr: true,
+      rollupOptions: {
+        input: ["custom-src/entry.fastify.tsx", "@qwik-city-plan"],
+      },
+      outDir: ".fastify",
+      minify: false,
+    },
+    plugins: [
+      nodeServerAdapter({
+        name: 'fastify-server',
+        ssg: {
+          include: ["/static/*"],
+          origin: "https://fastify.qwik.dev",
+        },
+      }),
+    ],
+  };
+});
