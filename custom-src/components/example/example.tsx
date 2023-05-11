5da0c6b837aae41252de32dee63af3f56a4b270a
Manu MA <manu.mtza@gmail.com>
2023-05-11 17:29:48 +0200

    🍤

--- custom-src/components/example/example.tsx
+++ custom-src/components/example/example.tsx
@@ -1,5 +1,6 @@
 import { component$, useStore } from '@builder.io/qwik';
 import { isBrowser, isDev, isServer } from '@builder.io/qwik/build';
+import styles from './example.module.css';
 
 export const ExampleTest = component$((props: { flag: boolean }) => {
   const state = useStore({
@@ -7,7 +8,7 @@ export const ExampleTest = component$((props: { flag: boolean }) => {
   });
 
   return (
-    <>
+    <div class={styles.example}>
       <span>Count:{state.counter}</span>
       <div class="icon">Flag: {props.flag ? '⭐' : '💣'}</div>
       <button class="btn-counter" onClick$={() => state.counter++}>
@@ -16,6 +17,6 @@ export const ExampleTest = component$((props: { flag: boolean }) => {
       <div id='is-browser'>isBrowser: {String(isBrowser)}</div>
       <div id='is-server'>isServer: {String(isServer)}</div>
       <div id='is-dev'>isDev: {String(isDev)}</div>
-    </>
+    </div>
   );
 });
