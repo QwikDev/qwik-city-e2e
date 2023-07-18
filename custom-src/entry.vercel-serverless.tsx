f4e45529d3053fcbfb68621c49656a8a051e6fd8
Adam Bradley <adamdbradley@users.noreply.github.com>
2023-07-17 20:03:15 -0500

    🐸

--- custom-src/entry.vercel-serverless.tsx
+++ custom-src/entry.vercel-serverless.tsx
@@ -1,6 +1,21 @@
-import { createQwikCity } from "@builder.io/qwik-city/middleware/vercel-edge";
+import { createQwikCity } from "@builder.io/qwik-city/middleware/node";
 import qwikCityPlan from "@qwik-city-plan";
 import { manifest } from "@qwik-client-manifest";
 import render from "./entry.ssr";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default createQwikCity({ render, qwikCityPlan, manifest });
+const { router, notFound, staticFile } = createQwikCity({
+  render,
+  qwikCityPlan,
+  manifest,
+});
+
+export default async function handler(req: VercelRequest, res: VercelResponse) {
+  staticFile(req, res, () => {
+    router(req, res, () => {
+      notFound(req, res, () => {
+        res.status(400);
+      });
+    });
+  });
+}
